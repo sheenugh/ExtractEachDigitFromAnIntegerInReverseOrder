7fe61ac15782fe7a6e177233dc1cdaf990f4940c
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 01:51:53 +0800

    adding a conclusion statement

--- extract_each_digit_from_integer_in_reverse_order.py
+++ extract_each_digit_from_integer_in_reverse_order.py
@@ -5,5 +5,6 @@ integer_num = "160504"
 # >>>>>>>>>> PSEUDO CODE <<<<<<<<<<
 # ----- ACTUAL CODES -----
 # - This section is for the for loop.
+print("If we are going to reverse the integer with a space, the result is:")
 for j in range(-1, -7, -1):
     print(str(integer_num[j]), " ",  end = " ")
