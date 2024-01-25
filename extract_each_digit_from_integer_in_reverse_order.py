b808159ff9e3fc70c01db71de12d0659a24c23a7
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 01:49:19 +0800

    tapos na mwehehehe

--- extract_each_digit_from_integer_in_reverse_order.py
+++ extract_each_digit_from_integer_in_reverse_order.py
@@ -1,5 +1,9 @@
 
 # ----- VARIABLES -----
+integer_num = "160504"
 
 # >>>>>>>>>> PSEUDO CODE <<<<<<<<<<
+# ----- ACTUAL CODES -----
 # - This section is for the for loop.
+for j in range(-1, -7, -1):
+    print(str(integer_num[j]), " ",  end = " ")
